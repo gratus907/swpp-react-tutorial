c8be6ef36ded2698b1d23ccdd01bccaffa48d466
gratus907 <gratus907@DESKTOP-6PE4G22.localdomain>
2020-09-17 19:53:10 +0900

    Push for relocating files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './containers/TodoList/TodoList';
+import Todo from './components/Todo/Todo';
 
 function App() {
   return (
     <div className="App">
+        <TodoList title={"Is change updated?"}></TodoList>
     </div>
   );
 }
